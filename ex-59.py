514d3a9bb0ef2317cba9bccd64fe5edc51f3d9ef
Luan Rodrigues <luanzragit@gmail.com>
2025-09-07 23:59:09 -0300

    ok

--- ex-59.py
+++ ex-59.py
@@ -1,13 +1,33 @@
 valores = []
 for i in range(2):
-    valor = float(input("Digite o valor: "))
-    valores.append(valor)
+        valor = float(input(f"Digite o {i+1}º valor: "))
+        valores.append(valor)
+while True:
+    seletor = int(input("""Escolha uma das opções: 
+[ 1 ] somar
 
-operacao  = input("Digite a operação 1 - Somar\n2 - Subtrair\n3 - Dividir\n4 - Multiplicar").strip()
-while operacao != ('1'or'2'or'3'or'4'):
-    operacao  = input("Digite a operação 1 - Somar\n2 - Subtrair\n3 - Dividir\n4 - Multiplicar")
+[ 2 ] multiplicar
 
-if operacao == '1':
-    soma = sum(valores)
-    print(soma)
-    
+[ 3 ] maior
+
+[ 4 ] novos números
+
+[ 5 ] sair do programa
+                        
+Digite aqui:                         
+"""))
+    if seletor == 1:
+        print(sum(valores))
+    elif seletor == 2:
+        print(valores[0]*valores[1])
+    elif seletor == 3:
+        print(max(valores))    
+    elif seletor == 4:
+        valores[0] = float(input("Digite o novo primeiro valor: "))
+        valores[1] = float(input("Digite o novo segundo valor: "))
+        print(valores[0])
+        print(valores[1])
+    elif seletor == 5:
+        break
+    else:
+        print("Digite algo no contexto do programa.")
