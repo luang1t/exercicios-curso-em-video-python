1bd44d25bad11b7da909437d60a064cd8a8d7232
luang1t <luanzragit@gmail.com>
2025-11-03 11:46:03 -0300

    100 mais otimizado

--- ex-100.py
+++ ex-100.py
@@ -1,23 +1,25 @@
 #Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar(). 
 # A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
 from random import randint
+from time import sleep
+
 
 numeros = []
 
-def sorteia(cont):
-    for _ in range(cont):
-        numero = randint(0,10)
+def sorteia(list):
+    print('Sorteando os valores na lista: ', end='')
+    for _ in range(0,5):
+        numero = randint(1,10)
         numeros.append(numero)
-def somaPar():
+        print(f'{numero} ', end='', flush = True)
+        sleep(0.3)
+    print('Pronto!')    
+def somaPar(list):
     soma = 0
-    for _ in range(len(numeros)):
-        if numeros[_]%2==0:
-            soma+=numeros[_]
-    return soma    
+    for numero in list:
+        if numero % 2 == 0:
+            soma+=numero
+    print(f'Somando os valores pares de {list}, temos {soma}')
 
-vezes = int(input("Digite quantas vezes: "))
-
-sorteia(vezes)
-print(numeros)
-print(somaPar())
-
+sorteia(numeros)
+somaPar(numeros)
