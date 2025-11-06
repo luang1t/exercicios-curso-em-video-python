ebae43a03b526b3e37503c655e34e4aec00fd771
luang1t <luanzragit@gmail.com>
2025-11-06 13:21:50 -0300

    testando prints

--- ex-102.py
+++ ex-102.py
@@ -3,14 +3,25 @@
 # indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
 from time import sleep
 
-def fatorial(num):
+def fatorial(num,show=False):
+    """
+    --> Calcula fatoial de um número.
+    :param num: Número a ser calcula o fatorial.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do fatorial de um número num.
+    """
     resultado = 1
-    print(f"{num}! = ",end= '')
+    
     for fat in range(num,0,-1):
-        print(f"{fat}x ", end = '',flush=True)
-        sleep(0.3)
+        if show:
+            print(f"{num}! = ",end= '')
+            if fat > 1:
+                print(f"{fat}x ", end = '',flush=True)
+                sleep(0.3)
+            else:
+                print(f"{fat}x ", end = '',flush=True)    
         resultado *= fat
     return resultado
 
 solicita = int(input("Digite um numero para saber seu fatorial: "))
-print(f" = {fatorial(solicita)}") 
+print(f" = {fatorial(solicita,show=True)}") 
